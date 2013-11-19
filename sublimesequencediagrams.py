2bcf6e77850bb6f79ac1eaac7a4114ccc8874662
Patrick Ayoup <patrick.ayoup@gmail.com>
2013-11-19 11:07:59 -0500

    Implemented get editor window contents

--- sublimesequencediagrams.py
+++ sublimesequencediagrams.py
@@ -1,5 +1,18 @@
 import sublime, sublime_plugin
 
-class ExampleCommand(sublime_plugin.TextCommand):
+DIAGRAM_STYLE = 'napkin'
+
+class WebSequenceDiagramsCommand(sublime_plugin.TextCommand):
+	'''
+	Generates a Web Sequence Diagram using the source code
+	in the editor.
+	'''
+
+	def _get_diagram_source():
+		'''Gets the source code of the diagram.'''
+		return self.view.substr(sublime.Region(0, self.view.size()))
+
 	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+		'''Executes the command'''
+		
+		

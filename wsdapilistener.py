e4b2cb1cd861ec3269967cddc192e3e232d75ff1
doublesharp <jsilver@doublesharp.com>
2015-11-19 21:29:35 -0800

    Update to work with Sublime Text 3/Python 3 * Support for apikey parameter * Get values from settings file * Show errors in quick menu * Syntax highlighting

--- wsdapilistener.py
+++ wsdapilistener.py
@@ -1,6 +1,5 @@
 import sublime
 import datetime
-import urllib2
 import webbrowser
 
 class ApiCallListener(object):
@@ -8,24 +7,15 @@ class ApiCallListener(object):
 
 	BASE_URL = 'http://www.websequencediagrams.com/{0}'
 
-	def _parse_errors(self, error_list):
-		'''Parses errors into a readable list.'''
-
-		return '\n'.join(error_list)
-
 	def _write_errors(self, errors):
 		'''Opens a new window and displays the errors.'''
 
-		#Create a new window and set the title with a timestamp.
-		active_window = sublime.active_window()
-		error_view = active_window.new_file()
-		timestamp = str(datetime.datetime.now()).split('.')[0]
-		error_view.set_name("wsdErrors-{0}.txt".format(timestamp))
+		#timestamp = str(datetime.datetime.now()).split('.')[0]
 
-		#Write the error list to the new window.
-		edit = error_view.begin_edit()
-		error_view.insert(edit, 0, errors)
-		error_view.end_edit(edit)
+		active_window = sublime.active_window()
+		l = list(errors)
+		l.insert(0, 'Errors occurred during save:')
+		active_window.show_quick_panel(l, self.on_select_error);
 
 	def _show_diagram(self, diagram_url):
 		'''Opens the generated diagram in the default web browser.'''
@@ -43,6 +33,9 @@ class ApiCallListener(object):
 				self._show_diagram(self.BASE_URL.format(wsd_api_call.result['img']))
 			else:
 				#Show an error message and display errors in a new window.
-				errors = self._parse_errors(wsd_api_call.result['errors'])
-				sublime.error_message('Diagram Generation Failed: There are errors in your source code.')
+				errors = wsd_api_call.result['errors']
+				#sublime.error_message('Diagram Generation Failed: There are errors in your source code.')
 				sublime.set_timeout(lambda: self._write_errors(errors), 100)
+
+	def on_select_error(self, index):
+		return;
